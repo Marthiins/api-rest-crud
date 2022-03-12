a567d2b775575bbf536cda47579509ed26541edf
Marthiins <sergio.marthiins@gmail.com>
2022-03-12 17:54:25 -0300

    Criando o Repository com Spring Data

--- springboot-rest-api-sample/src/main/java/br/com/marthiins/crud/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/marthiins/crud/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.marthiins.crud.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,20 +8,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marthiins.crud.repository.UsuarioRepository;
+
 /**
  *
  * A sample greetings controller to return greeting text
  */
 @RestController
 public class GreetingsController {
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
+ 
+	@Autowired /* Injeção de dependencias */
+	private UsuarioRepository usuarioRepository; /*usuario repository é uma variavel do java*/
+	
 }
