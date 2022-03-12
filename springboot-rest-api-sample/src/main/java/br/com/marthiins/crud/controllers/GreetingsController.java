a5ce3452d3a7acac674d2892bf41d3a80329ea50
Marthiins <sergio.marthiins@gmail.com>
2022-03-12 17:57:46 -0300

    Criando END-POINT de listar todos usuários

--- springboot-rest-api-sample/src/main/java/br/com/marthiins/crud/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/marthiins/crud/controllers/GreetingsController.java
@@ -1,13 +1,15 @@
 package br.com.marthiins.crud.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marthiins.crud.model.Usuario;
 import br.com.marthiins.crud.repository.UsuarioRepository;
 
 /**
@@ -19,5 +21,16 @@ public class GreetingsController {
  
 	@Autowired /* Injeção de dependencias */
 	private UsuarioRepository usuarioRepository; /*usuario repository é uma variavel do java*/
+	
+	@GetMapping(value = "listatodos") /* Buscar da API buscar todos */
+	@ResponseBody /* Retorna os dados para o corpo da resposta */
+	public ResponseEntity<List<Usuario>> listaUsuario() {
+
+		List<Usuario> usuarios = usuarioRepository.findAll();/* Executa a consulta no banco de dados */
+
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+	
+	
 	
 }
